83a77edf705276362af206eae9d1b247dd7fca42
tanzid-48 <mdtanzid.525@gmail.com>
2026-04-08 01:53:45 +0600

    Add tarnary Condition to show WishList And ReadList

--- src/components/ListBook/ReadList.jsx
+++ src/components/ListBook/ReadList.jsx
@@ -3,14 +3,20 @@ import { BookContext } from '../../context/BookContext';
 import ReadListCard from './ReadListCard';
 
 const ReadList = () => {
- 
-const { storedBooks } = useContext(BookContext);
 
+    const { storedBooks } = useContext(BookContext);
 
     return (
         <div>
             {
-                storedBooks.map( book => <ReadListCard key={book.bookId} book = {book}></ReadListCard> )
+                storedBooks.length === 0 ? (<div className="h-[60vh] flex flex-col items-center justify-center bg-gray-50 rounded-3xl">
+                    <div className="text-6xl mb-4">📚</div>
+                    <h3 className="text-3xl font-bold text-gray-700">No Read Books Yet</h3>
+                    <p className="text-gray-500 mt-3 max-w-md text-center">
+                        Books you mark as "Read" will appear here.
+                    </p>
+                </div>)
+                    : (storedBooks.map(book => <ReadListCard key={book.bookId} book={book}></ReadListCard>))
             }
         </div>
     );
--- src/components/ListBook/WishList.jsx
+++ src/components/ListBook/WishList.jsx
@@ -6,10 +6,20 @@ const WishList = () => {
 
     const { wishListBooks } = useContext(BookContext);
     return (
-        <div>
-            {
-                wishListBooks.map(book => <WishListCard key={book.bookId} book={book}></WishListCard>)
-            }
+     <div>
+    {
+        wishListBooks.length === 0 
+        ? (
+            <div className="h-[60vh] flex flex-col items-center justify-center bg-gray-50 rounded-3xl">
+        <div className="text-6xl mb-4">📚</div>
+        <h3 className="text-3xl font-bold text-gray-700">No Wish Books Yet</h3>
+        <p className="text-gray-500 mt-3 max-w-md text-center">
+            Books you mark as "Wish List" will appear here.
+        </p>
+      </div>
+        ) : ( wishListBooks.map(book => <WishListCard key={book.bookId} book={book}></WishListCard>))
+
+    }
         </div>
     );
 };
