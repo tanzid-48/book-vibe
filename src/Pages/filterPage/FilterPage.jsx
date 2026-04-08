a13e96833ae8d10c9a41b1bf5af9b2281be1a3d5
tanzid-48 <mdtanzid.525@gmail.com>
2026-04-08 18:40:48 +0600

    Fix same bug

---
+++ src/Pages/filterPage/FilterPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FilterPage = () => {
+    return (
+        <div>
+            <h3>Hello</h3>
+        </div>
+    );
+};
+
+export default FilterPage;<h3>Hello</h3>
